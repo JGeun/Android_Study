418a1a8c44765b6a188e197134160e9719dba394
jgeun <pjk8926@gmail.com>
2022-07-14 21:25:37 +0900

    Clean ViewHolders

--- CommonListAdapter/app/src/main/java/jgeun/study/viewholder/OneImageViewHolder.kt
+++ CommonListAdapter/app/src/main/java/jgeun/study/viewholder/OneImageViewHolder.kt
@@ -2,16 +2,16 @@ package jgeun.study.viewholder
 
 import com.bumptech.glide.Glide
 import jgeun.study.commonlistadapter.data.CommonItem
+import jgeun.study.commonlistadapter.data.viewobject.OneImageViewObject
 import jgeun.study.commonlistadapter.databinding.ItemOneImageBinding
-import jgeun.study.commonlistadapter.my.ViewObject
 
 class OneImageViewHolder(
     private val binding: ItemOneImageBinding
 ) : CommonViewHolder(binding) {
     override fun bind(item: CommonItem) {
-        val viewObject = item.viewObject as ViewObject.OneImageViewObject
+        val viewObject = item.viewObject as OneImageViewObject
         Glide.with(binding.root)
-            .load(viewObject.imageVO.url)
+            .load(viewObject.url)
             .into(binding.image)
     }
 }
--- CommonListAdapter/app/src/main/java/jgeun/study/viewholder/TwoLineTextViewHolder.kt
+++ CommonListAdapter/app/src/main/java/jgeun/study/viewholder/TwoLineTextViewHolder.kt
@@ -1,14 +1,14 @@
 package jgeun.study.viewholder
 
 import jgeun.study.commonlistadapter.data.CommonItem
+import jgeun.study.commonlistadapter.data.viewobject.TwoLineTextViewObject
 import jgeun.study.commonlistadapter.databinding.ItemTwoLineTextBinding
-import jgeun.study.commonlistadapter.my.ViewObject
 
 class TwoLineTextViewHolder(
     private val binding: ItemTwoLineTextBinding
 ) : CommonViewHolder(binding) {
     override fun bind(item: CommonItem) {
-        val viewObject = item.viewObject as ViewObject.TwoLineTextViewObject
+        val viewObject = item.viewObject as TwoLineTextViewObject
         binding.title.text = viewObject.title
         binding.content.text = viewObject.contents
     }
