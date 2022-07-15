cc800bdb601f38ebcaeafcfec690e68aa4c6c037
JGeun <pjk8926@gmail.com>
2022-07-15 17:55:17 +0900

    Navigation_Java_Study finish

---
+++ Inflearn_Jetpack/navigation_java/app/src/main/java/jgeun/study/navigation_java/SecondFragment.java
@@ -0,0 +1,35 @@
+package jgeun.study.navigation_java;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class SecondFragment extends Fragment {
+
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        String text = SecondFragmentArgs.fromBundle(getArguments()).getVar();
+        TextView textView = view.findViewById(R.id.second_text);
+        textView.setText(text);
+    }
+}
