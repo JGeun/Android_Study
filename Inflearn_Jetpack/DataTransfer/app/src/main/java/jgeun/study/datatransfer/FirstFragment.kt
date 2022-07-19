6299439f3183de79280a257f6201dec322366d71
JGeun <pjk8926@gmail.com>
2022-07-19 15:15:20 +0900

    MultiplePermission With RegisterContracts

--- Inflearn_Jetpack/DataTransfer/app/src/main/java/jgeun/study/datatransfer/FirstFragment.kt
+++ Inflearn_Jetpack/DataTransfer/app/src/main/java/jgeun/study/datatransfer/FirstFragment.kt
@@ -1,46 +1,35 @@
 package jgeun.study.datatransfer
 
-import android.content.Intent
+import android.Manifest
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.os.bundleOf
-import androidx.fragment.app.*
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 
 class FirstFragment : Fragment(R.layout.fragment_first) {
 
+    private val requestPermission = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { map ->
+        if (map[Manifest.permission.ACCESS_FINE_LOCATION]!!) {
+            Toast.makeText(requireContext(), "ACCESS_FINE_LOCATION 성공", Toast.LENGTH_SHORT).show()
+        }
 
+        if (map[Manifest.permission.ACCESS_COARSE_LOCATION]!!) {
+            Toast.makeText(requireContext(), "ACCESS_COARSE_LOCATION 성공", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-        val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) {
-            view.findViewById<ImageView>(R.id.imageView).setImageURI(it)
-        }
-
-        val getStartActivityForResult = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult()
-        ) { activityResult ->
-            activityResult.data?.let { intent ->
-                intent.extras?.let { bundle ->
-                    Log.d("FirstFragment", "result: : ${bundle.getString("data", "world")}")
-                }
-            }
-        }
-
         view.findViewById<Button>(R.id.button).setOnClickListener {
-            // MIME TYPE
-            Intent(requireActivity(), ResultActivity::class.java).apply {
-                getStartActivityForResult.launch(this)
-            }
+            requestPermission.launch(arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+            )
         }
     }
 }
