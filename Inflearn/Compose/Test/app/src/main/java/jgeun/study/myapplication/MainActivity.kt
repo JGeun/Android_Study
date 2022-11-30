e39645d1cea41a775c968f042bd59a1ddd6d0015
JGeun <pjk8926@gmail.com>
2022-11-30 18:23:35 +0900

    Compose Navigation

--- Inflearn/Compose/Test/app/src/main/java/jgeun/study/myapplication/MainActivity.kt
+++ Inflearn/Compose/Test/app/src/main/java/jgeun/study/myapplication/MainActivity.kt
@@ -3,78 +3,104 @@ package jgeun.study.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            var isFavorite by rememberSaveable {
-                mutableStateOf(false)
-            }
-            ImageCard(
-                modifier = Modifier.fillMaxWidth(0.5f)
-                    .padding(16.dp),
-                isFavorite = isFavorite) { favorite ->
-                isFavorite = favorite
+            val navController = rememberNavController()
+
+            NavHost(navController = navController, startDestination = "first") {
+                composable("first") {
+                    FirstScreen(navController)
+                }
+                composable("second") {
+                    SecondScreen(navController)
+                }
+                composable("third/{value}") { backStackEntry ->
+                    ThirdScreen(
+                        navController = navController,
+                        value = backStackEntry.arguments?.getString("value") ?: "")
+                }
             }
         }
     }
 }
 
 @Composable
-fun ImageCard(
-    modifier: Modifier = Modifier,
-    isFavorite: Boolean,
-    onTabFavorite: (Boolean) -> Unit,
-) {
-    Card(
-        modifier = modifier,
-        shape = RoundedCornerShape(8.dp),
-        elevation = 5.dp
+fun FirstScreen(navController: NavController) {
+    val (value, setValue) = remember {
+        mutableStateOf("")
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Box(
-            modifier = Modifier.height(200.dp)
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.poster2),
-                contentDescription = "poster",
-                contentScale = ContentScale.Crop,
-            )
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.TopEnd
-            ) {
-                IconButton(onClick = {
-                    onTabFavorite(!isFavorite)
-                }) {
-                    Icon(imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
-                        contentDescription = "favorite",
-                        tint = Color.White
-                    )
-                }
-            }
+        Text(text = "첫 화면")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            navController.navigate("second")
+        }) {
+            Text("두 번째!")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(value = value, onValueChange = setValue)
+        Button(onClick = {
+        if (value.isNotEmpty()) {
+            navController.navigate("third/$value")
+        }
+        }) {
+            Text("세 번째!")
+        }
+    }
+}
+
+@Composable
+fun SecondScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "두 번째 화면")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            navController.navigateUp()
+        }) {
+            Text("뒤로 가기!")
+        }
+    }
+}
+
+@Composable
+fun ThirdScreen(navController: NavController, value: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "세 번째 화면")
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(value)
+        Button(onClick = {
+            navController.navigateUp()
+        }) {
+            Text("뒤로 가기!")
         }
     }
 }
