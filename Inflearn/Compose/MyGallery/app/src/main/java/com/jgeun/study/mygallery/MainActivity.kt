318a1b906b907a161fd4258337cc0a3b9afe7356
JGeun <pjk8926@gmail.com>
2022-12-14 20:29:16 +0900

    MyGallery

---
+++ Inflearn/Compose/MyGallery/app/src/main/java/com/jgeun/study/mygallery/MainActivity.kt
@@ -0,0 +1,174 @@
+package com.jgeun.study.mygallery
+
+import android.Manifest
+import android.app.Application
+import android.content.ContentUris
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.webkit.PermissionRequest
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberImagePainter
+import com.google.accompanist.pager.*
+import com.jgeun.study.mygallery.ui.theme.MyGalleryTheme
+import kotlin.math.absoluteValue
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalPagerApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            val viewModel = viewModel<MainViewModel>()
+            var granted by remember { mutableStateOf(false) }
+
+            val launcher = rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                granted = isGranted
+            }
+
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED) {
+                granted = true
+            }
+
+            if (granted) {
+                viewModel.fetchPhotos()
+                HomeScreen(photoUris = viewModel.photoUris.value)
+            } else {
+                PermissionRequestScreen {
+                    launcher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            }
+        }
+    }
+}
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val _photoUris = mutableStateOf(emptyList<Uri>())
+    val photoUris: State<List<Uri>> = _photoUris
+
+    fun fetchPhotos() {
+        val uris = mutableListOf<Uri>()
+
+        getApplication<Application>().contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            null,
+            null,
+            null,
+            "${MediaStore.Images.ImageColumns.DATE_TAKEN} DESC"
+        )?.use { cursor ->
+            val idIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(idIndex)
+
+                val contentUri = ContentUris.withAppendedId(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    id
+                )
+
+                uris.add(contentUri)
+            }
+
+            _photoUris.value = uris
+
+        }
+    }
+}
+
+@Composable
+fun PermissionRequestScreen(onClick: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text("권한이 허용되지 않았습니다")
+        Button(onClick = onClick) {
+            Text("권한 요청")
+        }
+    }
+}
+
+@ExperimentalPagerApi
+@Composable
+fun HomeScreen(photoUris: List<Uri>) {
+    val pagerState = rememberPagerState()
+
+    Column(Modifier.fillMaxSize()) {
+        HorizontalPager(
+            state = pagerState,
+            count = photoUris.size,
+            modifier = Modifier
+                .weight(1f)
+                .padding(16.dp)
+                .fillMaxSize()
+        ) { pageIndex ->
+            Card(
+                modifier = Modifier.
+                        graphicsLayer {
+                            val pageOffset = calculateCurrentOffsetForPage(pageIndex).absoluteValue
+
+                            lerp(
+                                start = 0.85f,
+                                stop = 1f,
+                                fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                            ).also { scale ->
+                                scaleX = scale
+                                scaleY = scale
+                            }
+
+                            alpha = lerp(
+                                start = 0.5f,
+                                stop = 1f,
+                                fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                            )
+                        }
+            ) {
+                Image(
+                    painter =    rememberImagePainter(
+                        data = photoUris[pageIndex]
+                    ),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
+        
+        HorizontalPagerIndicator(
+            pagerState = pagerState,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(16.dp)
+        )
+    }
+}
+
+private fun lerp(start: Float, stop: Float, fraction: Float): Float =
+    (1 - fraction) * start + fraction * stop
