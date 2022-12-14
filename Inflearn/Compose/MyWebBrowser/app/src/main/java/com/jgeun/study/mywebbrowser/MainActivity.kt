40872a329ffbb288cc30d87edc66510b4b91ca15
JGeun <pjk8926@gmail.com>
2022-12-14 20:38:42 +0900

    MyBrowser

---
+++ Inflearn/Compose/MyWebBrowser/app/src/main/java/com/jgeun/study/mywebbrowser/MainActivity.kt
@@ -0,0 +1,151 @@
+package com.jgeun.study.mywebbrowser
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.jgeun.study.mywebbrowser.ui.theme.MyWebBrowserTheme
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val viewModel = viewModel<MainViewModel>()
+            HomeScreen(viewModel = viewModel)
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(viewModel: MainViewModel) {
+    val focusManager = LocalFocusManager.current
+
+    val (inputUrl, setUrl) = rememberSaveable {
+        mutableStateOf("Https://www.google.com")
+    }
+    val scaffoldState = rememberScaffoldState()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "나만의 웹 브라우저") },
+                actions = {
+                    IconButton(onClick = {
+                        viewModel.undo()
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "back",
+                            tint = Color.White
+                        )
+                    }
+                    IconButton(onClick = {
+                        viewModel.redo()
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowForward,
+                            contentDescription = "forward",
+                            tint = Color.White
+                        )
+                    }
+                }
+            )
+        },
+        scaffoldState = scaffoldState
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize()
+        ) {
+            OutlinedTextField(
+                value = inputUrl,
+                onValueChange = setUrl,
+                label = { Text("https://") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+                keyboardActions = KeyboardActions(onSearch = {
+                    viewModel.url.value = inputUrl
+                    focusManager.clearFocus()
+                })
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+            MyWebView(
+                viewModel = viewModel,
+                scaffoldState = scaffoldState
+            )
+        }
+    }
+}
+
+@Composable
+fun MyWebView(
+    viewModel: MainViewModel,
+    scaffoldState: ScaffoldState,
+) {
+    val webView = rememberWebView()
+
+    LaunchedEffect(Unit) {
+        viewModel.undoSharedFlow.collectLatest {
+            if (webView.canGoBack()) {
+                webView.goBack()
+            } else {
+                scaffoldState.snackbarHostState.showSnackbar("더 이상 뒤로 갈 수 없음")
+            }
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        viewModel.redoSharedFlow.collectLatest {
+            if (webView.canGoForward()) {
+                webView.goForward()
+            } else {
+                scaffoldState.snackbarHostState.showSnackbar("더 이상 앞으로 갈 수 없음")
+            }
+        }
+    }
+
+    AndroidView(
+        modifier = Modifier.fillMaxSize(),
+        factory = { webView },
+        update = { webView ->
+            webView.loadUrl(viewModel.url.value)
+        }
+    )
+}
+
+@Composable
+fun rememberWebView(): WebView {
+    val context = LocalContext.current
+    val webView = remember {
+        WebView(context).apply {
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+            loadUrl("https://google.com")
+        }
+    }
+    return webView
+}
---
+++ Inflearn/Compose/MyWebBrowser/app/src/main/java/com/jgeun/study/mywebbrowser/MainViewModel.kt
@@ -0,0 +1,30 @@
+package com.jgeun.study.mywebbrowser
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    val url = mutableStateOf("https://www.google.com")
+
+    private val _undoSharedFlow = MutableSharedFlow<Boolean>()
+    val undoSharedFlow = _undoSharedFlow.asSharedFlow()
+
+    private val _redoSharedFlow = MutableSharedFlow<Boolean>()
+    val redoSharedFlow = _redoSharedFlow.asSharedFlow()
+
+    fun undo() {
+        viewModelScope.launch {
+            _undoSharedFlow.emit(true)
+        }
+    }
+
+    fun redo() {
+        viewModelScope.launch {
+            _redoSharedFlow.emit(true)
+        }
+    }
+}
